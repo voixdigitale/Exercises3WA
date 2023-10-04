85b2c789622a16050c52231394947378acf88daa
voixdigitale <cristian@voixdigitale.com>
2023-10-04 13:56:36 +0200

    Changing project

---
+++ Classement/Classement.cs
@@ -0,0 +1,21 @@
+﻿string[] classement = new string[8] {
+    "Ryu",
+    "Sagat",
+    "Chun-Li",
+    "Ken",
+    "Guile",
+    "E. Honda",
+    "Blanka",
+    "Zangief"
+};
+
+Console.WriteLine("Veuillez entrer une place entre 1 et 8 et appuyez sur ENTRER pour consulter le classement :");
+int place = Int32.Parse(Console.ReadLine());
+
+string position = place switch {
+    1 => "ère",
+    2 => "nde",
+    _ => "ème"
+};
+
+Console.WriteLine($"{classement[place-1]} est arrivé à la {place}{position} place.");
