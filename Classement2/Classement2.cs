57e01a6468c570bb77e270e2f630c9d1987c5d0b
voixdigitale <cristian@voixdigitale.com>
2023-10-10 10:33:44 +0200

    Classement et loto

---
+++ Classement2/Classement2.cs
@@ -0,0 +1,23 @@
+﻿string[] classement = new string[8] {
+    "Ryu",
+    "Sagat",
+    "Chun-Li",
+    "Ken",
+    "Guile",
+    "E. Honda",
+    "Blanka",
+    "Zangief"
+};
+
+Console.WriteLine("Voici le classement :\n");
+
+
+for (int i = 0; i < classement.Length; i++) {
+    string position = (i + 1) switch {
+        1 => "er",
+        2 => "nd",
+        _ => "ème"
+    };
+
+    Console.WriteLine($"{i + 1}{position} : {classement[i]}");
+}
---
+++ Loto/Loto.cs
@@ -0,0 +1,21 @@
+﻿int[] loto = new int[6];
+for ( int i = 0; i < loto.Length; i++ ) {
+    int tirage = tirageEntre1Et50();
+    bool tirageUnique = !Array.Exists(loto, nombre => nombre == tirage);
+    while (!tirageUnique) {
+        tirage = tirageEntre1Et50();
+        tirageUnique = !Array.Exists(loto, nombre => nombre == tirage);
+    }
+    loto[i] = tirage;
+}
+
+Console.WriteLine("Voici les 6 nombres uniques entre 1 et 50 :");
+
+for (int i = 0; i < loto.Length; i++) {
+    Console.WriteLine(loto[i]);
+}
+
+int tirageEntre1Et50() {
+    Random rand = new Random();
+    return rand.Next(0, 51);
+}
